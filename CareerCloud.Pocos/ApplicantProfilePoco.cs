2d1a4b00246cf1caeb4161181c0d10e647564b10
Suraj Sharma <surajkumarsharma@gmail.com>
2018-05-20 13:38:42 -0400

    Created App_Profile

---
+++ CareerCloud.Pocos/ApplicantProfilePoco.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CareerCloud.Pocos
+{
+    [Table("Applicant_Profile")]
+    public class ApplicantProfilePoco
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public Guid Login { get; set; }
+        [Column("Current_Salary")]
+        public decimal CurrentSalary { get; set; }
+        [Column("Current_Rate")]
+        public decimal CurrentRate { get; set; }
+        public char[] Currency { get; set; }
+        [Key]
+        public Guid CountryCode { get; set; }
+        [Column("State_Province_Code")]
+        public char[] StateProvinceCode { get; set; }
+        [Column("Street_Address")]
+        public string StreetAddress { get; set; }
+        [Column("City_Town")]
+        public string CityTown { get; set; }
+        [Column("Zip_Postal_Code")]
+        public char[] ZipPostalCode { get; set; }
+        [Column("Time_Stamp")]
+        public byte[] TimeStamp { get; set; }
+
+
+    }
+}
