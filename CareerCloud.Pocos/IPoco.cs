3e3c44c3f33861dc9b75765f162f2d3a26138959
Suraj Sharma <surajkumarsharma@gmail.com>
2018-05-26 10:07:47 -0400

    Added UnitTest Project

---
+++ CareerCloud.Pocos/IPoco.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CareerCloud.Pocos
+{
+    public interface IPoco
+    {
+        Guid Id { get; set; }
+
+    }
+}
