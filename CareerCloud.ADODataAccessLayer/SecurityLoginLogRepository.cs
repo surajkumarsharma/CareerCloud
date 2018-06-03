756ba40cc06834c0bb10cb943755739f0cb22c68
Suraj Sharma <surajkumarsharma@gmail.com>
2018-06-03 16:02:59 -0400

    Completed_ADOClasses

---
+++ CareerCloud.ADODataAccessLayer/SecurityLoginLogRepository.cs
@@ -0,0 +1,48 @@
+﻿using CareerCloud.DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CareerCloud.ADODataAccessLayer
+{
+    class SecurityLoginLogRepository : IDataRepository<SecurityLoginLogPoco>
+    {
+        public void Add(params SecurityLoginLogPoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<SecurityLoginLogPoco> GetAll(params Expression<Func<SecurityLoginLogPoco, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<SecurityLoginLogPoco> GetList(Expression<Func<SecurityLoginLogPoco, bool>> where, params Expression<Func<SecurityLoginLogPoco, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SecurityLoginLogPoco GetSingle(Expression<Func<SecurityLoginLogPoco, bool>> where, params Expression<Func<SecurityLoginLogPoco, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(params SecurityLoginLogPoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(params SecurityLoginLogPoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CareerCloud.ADODataAccessLayer/SecurityRoleRepository.cs
@@ -0,0 +1,48 @@
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CareerCloud.ADODataAccessLayer
+{
+    class SecurityRoleRepository : IDataRepository<SecurityRolePoco>
+    {
+        public void Add(params SecurityRolePoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<SecurityRolePoco> GetAll(params System.Linq.Expressions.Expression<Func<SecurityRolePoco, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<SecurityRolePoco> GetList(System.Linq.Expressions.Expression<Func<SecurityRolePoco, bool>> where, params System.Linq.Expressions.Expression<Func<SecurityRolePoco, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SecurityRolePoco GetSingle(System.Linq.Expressions.Expression<Func<SecurityRolePoco, bool>> where, params System.Linq.Expressions.Expression<Func<SecurityRolePoco, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(params SecurityRolePoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(params SecurityRolePoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
