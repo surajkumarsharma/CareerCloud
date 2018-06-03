71c22773d35d98b1abf11de2c56d5e4e67f031fc
Suraj Sharma <surajkumarsharma@gmail.com>
2018-06-03 12:56:25 -0400

    CareerCloud_ADOLayer_uptoApplicantSkill

---
+++ CareerCloud.ADODataAccessLayer/ApplicantEducationRepository.cs
@@ -0,0 +1,49 @@
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CareerCloud.ADODataAccessLayer
+{
+    class ApplicantEducationRepository : IDataRepository<ApplicantEducationPoco>
+    {
+        public void Add(params ApplicantEducationPoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<ApplicantEducationPoco> GetAll(params Expression<Func<ApplicantEducationPoco, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<ApplicantEducationPoco> GetList(Expression<Func<ApplicantEducationPoco, bool>> where, params Expression<Func<ApplicantEducationPoco, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ApplicantEducationPoco GetSingle(Expression<Func<ApplicantEducationPoco, bool>> where, params Expression<Func<ApplicantEducationPoco, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(params ApplicantEducationPoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(params ApplicantEducationPoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CareerCloud.ADODataAccessLayer/ApplicantJobApplicationRepository.cs
@@ -0,0 +1,48 @@
+﻿using CareerCloud.DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CareerCloud.ADODataAccessLayer
+{
+    class ApplicantJobApplicationRepository : IDataRepository<ApplicantJobApplicationRepository>
+    {
+        public void Add(params ApplicantJobApplicationRepository[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<ApplicantJobApplicationRepository> GetAll(params Expression<Func<ApplicantJobApplicationRepository, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<ApplicantJobApplicationRepository> GetList(Expression<Func<ApplicantJobApplicationRepository, bool>> where, params Expression<Func<ApplicantJobApplicationRepository, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ApplicantJobApplicationRepository GetSingle(Expression<Func<ApplicantJobApplicationRepository, bool>> where, params Expression<Func<ApplicantJobApplicationRepository, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(params ApplicantJobApplicationRepository[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(params ApplicantJobApplicationRepository[] items)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
